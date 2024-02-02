c41a2be967ad022be0d49523ab2ec634008a8bee
thiagopdo <thiago.falconer@gmail.com>
2024-02-02 17:03:27 -0300

    implemented AppLayout and nested on App as children

---
+++ src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <div>
+      <Link to="/">Fast React Pizza Co.</Link>
+
+      <p>Thiago</p>
+    </div>
+  );
+}
+
+export default Header;
