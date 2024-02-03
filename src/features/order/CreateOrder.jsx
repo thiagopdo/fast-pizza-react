61a020e8c825fea8fc595e9cc93794677df08349
thiagopdo <thiago.falconer@gmail.com>
2024-02-03 06:20:51 -0300

    added error handling form phone number

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 
 import { useState } from "react";
-import { Form, redirect, useNavigation } from "react-router-dom";
+import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
@@ -37,6 +37,9 @@ const fakeCart = [
 function CreateOrder() {
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submitting";
+
+  const formErrors = useActionData();
+
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
 
@@ -55,6 +58,7 @@ function CreateOrder() {
           <div>
             <input type="tel" name="phone" required />
           </div>
+          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
         <div>
@@ -102,12 +106,22 @@ export async function action({ request }) {
     priority: data.priority === "on",
   };
 
+  //checks if phone number is valid
+  const errors = {};
+  if (!isValidPhone(order.phone))
+    errors.phone =
+      "Please add your phone number, so we might need it to contact you";
+
+  if (Object.keys(errors).length > 0) return errors;
+
   /**
    * submits the new order object with a POST request with createOrder
    */
   const newOrder = await createOrder(order);
 
-  //redirects to the page order with an ID after getting back the newOrder
+  /**
+   * redirects to the page order with an ID after getting back the newOrder
+   */
   return redirect(`/order/${newOrder.id}`);
 }
 
