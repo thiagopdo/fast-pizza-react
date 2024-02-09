d80ccba4932c3b0f5b019f373eeff2d51efc5da9
thiagopdo <thiago.falconer@gmail.com>
2024-02-09 10:28:32 -0300

    updated cart with API data

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -4,6 +4,7 @@ import LinkButton from "../../ui/LinkButton";
 import Button from "../../ui/Button";
 import CartItem from "./CartItem";
 import { useSelector } from "react-redux";
+import { getCart } from "./cartSlice.js";
 
 const fakeCart = [
   {
@@ -31,7 +32,7 @@ const fakeCart = [
 
 function Cart() {
   const username = useSelector((state) => state.user.username);
-  const cart = fakeCart;
+  const cart = useSelector(getCart);
 
   return (
     <div className="px-4 py-3">
